2f6098ddf4ebe45c04d724f8a747f5eb9f1a712c
Bulle1982 <bullites114@hotmail.com>
2023-12-07 10:25:23 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -34,24 +34,28 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-
+    # Message to the user in the terminal
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
+     # Data input from the user stored in the variable data_str.
     data_str = input("Enter your data here: ")
-    # print(f"The data provided is: {data_str}")
+    # split the data_str and store it in a variable called sales_data.
     sales_data = data_str.split(",")
+    #Call the function validate_data and pass sales_data.
     validate_data(sales_data)
-    #print(sales_data)
+   
 
 def validate_data(values):
     """
-    Inside the try, converts all the string values into integers.
+    Inside the try, all the string values are converted into integers.
     Raises ValuError if strings cannot be converted into int,
     or if there are no exactly 6 values
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
