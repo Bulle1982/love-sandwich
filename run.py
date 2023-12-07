670800609c072309e6f4235a3ac4c5231a316985
Bulle1982 <bullites114@hotmail.com>
2023-12-07 09:44:51 +0000

    add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwich")
 
+"""
 sales = SHEET.worksheet("sales")
 sales_data = sales.get_all_values()
 print("Below is the sales data:")
@@ -27,8 +28,35 @@ surplus_data = sales.get_all_values()
 print("Below is the surplus data:")
 print(surplus_data)
 
+"""
 
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
 
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    # print(f"The data provided is: {data_str}")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+    #print(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all the string values into integers.
+    Raises ValuError if strings cannot be converted into int,
+    or if there are no exactly 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+                print(f"Invalid data: {e}, please try again.\n")
+
+get_sales_data()    
